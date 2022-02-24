2ef1edaf6d9da319a15dbdca57efdbbb5c853993
Nelson Rivera <isaacriverahn7@gmail.com>
2022-02-23 18:17:01 -0600

    Ejercicio de campos calculados y despliegue de la barra progresiva

--- models/session.py
+++ models/session.py
@@ -13,6 +13,19 @@ class open_academy(models.Model):
     duration = fields.Float(digits=(5,2))
     description = fields.Text(string='Description')
     cupos = fields.Integer(string='Numero de cupos de la sesion')
-    instructor_id = fields.Many2one('res.partner', ondelete="set null", string='Instructor', domain=[('instructor', '=', True)])
+    instructor_id = fields.Many2one('res.partner', ondelete="set null", string='Instructor', domain=['|',('instructor', '=', True),
+                                                                                                            ('category_id.name','ilike',"Teacher")])
     course_id= fields.Many2one('open_academy.course', required=True, string='Curso_Id')
     attendee_id= fields.Many2many('res.partner', strings='Attendees')
+
+    asistencia = fields.Float(string="Cupos ocupados", compute='_asistencia')
+
+    #Porcentaje de los asientos ocupados
+    @api.depends('cupos','attendee_id')
+    def _asistencia(self):
+        for i in self:
+            if not i.cupos:
+                i.asistencia = 0.0
+            else:
+                i.asistencia = 100.0*len(i.attendee_id)/i.cupos
+    
