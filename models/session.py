6218af3281ff3eec53868696e7b06caf6409e2e6
Nelson Rivera <isaacriverahn7@gmail.com>
2022-02-23 21:17:20 -0600

    Ejercicio Calendario y decoracion de vistas

--- models/session.py
+++ models/session.py
@@ -5,6 +5,7 @@ from email.policy import default
 from string import digits
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
+from datetime import timedelta
 
 
 class open_academy(models.Model):
@@ -23,6 +24,7 @@ class open_academy(models.Model):
     activo= fields.Boolean(default=True)
 
     asistencia = fields.Float(string="Cupos ocupados", compute='_asistencia')
+    fecha_final = fields.Date(string="Fecha final", store=True, compute='_obtener_fecha_final', inverse='_establecer_fecha_final')
 
     #Porcentaje de los asientos ocupados
     @api.depends('cupos','attendee_id')
@@ -58,3 +60,21 @@ class open_academy(models.Model):
             if record == self.instructor_id:
                 raise ValidationError("Un instructor no puede formar parte de la lista de asistentes")
 
+    @api.depends('duration', 'start_date')
+    def _obtener_fecha_final(self):
+        for record in self:
+            if not(record.start_date and record.duration):
+                record.fecha_final = record.start_date
+                continue
+            comienzo = fields.Datetime.from_string(record.start_date)
+            duracion = timedelta(days=record.duration, seconds=-1)
+            record.fecha_final = comienzo + duracion
+    
+    def _establecer_fecha_final(self):
+        for record in self:
+            if not (record.start_date and record.fecha_final):
+                continue
+
+            fecha_inicial = fields.Datetime.from_string(record.start_date)
+            fecha_final = fields.Datetime.from_string(record.fecha_final)
+            record.duration = (fecha_final-fecha_inicial).days + 1
